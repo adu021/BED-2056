5554742f1283eecf319fb50310086849246edd9b
Aymeric Dufossé <adu021@uit.no>
2019-11-18 15:50:45 +0100

    T4 Final

--- Task4/T4script.R
+++ Task4/T4script.R
@@ -2,6 +2,7 @@ library(dplyr)
 library(lubridate)
 library(tidyverse)
 library(ggthemes)
+library(forecast)
 
 storedata <- read.csv("Task4/storedata.csv")
 
@@ -26,3 +27,18 @@ table2 <- table2 %>% filter(Region_13 > Region_1)
 table3 <- storedata %>% filter(year(Order_Date) == 2017, Region != "Region 3", Region != "Region 5", Region != "Region 8") %>% group_by(Customer_Segment, Product_Category) %>% summarize(Average_Profit = mean(Profit))
 maxtable3 <- table3 %>% ungroup() %>% filter(Average_Profit == max(Average_Profit))
 ggplot(table3, aes(x = Product_Category, y = Average_Profit, fill=Product_Category)) + geom_histogram(stat="identity") + facet_wrap(~Customer_Segment) + ggtitle("Average profit by customer segment and product category in 2017") + ylab("Average Profit") + labs(fill = "Product Category") + theme_stata() + theme(axis.title.x = element_blank(), axis.text.x = element_blank(), axis.ticks.x = element_blank()) + scale_fill_brewer(palette = "Greens")
+
+Model_List <- 
+  list(p = c(0,1,2,3,4),
+       q = c(0,1,2,3,4),
+       d = c(0,1),
+       P = c(0,1,2,3,4),
+       Q = c(0,1,2,3,4),
+       D = c(0,1),
+       S = 12,
+       sep = c(", ")) %>% 
+  cross() %>% map(lift(paste))
+model1416 <- storedata %>% filter(year(Order_Date) < 2017) %>% filter(Customer_Segment == "Small Business" & Product_Category == "Office Supplies")
+auto.arima(model1416$Order_Quantity, max.p = 4, max.q = 4, max.P = 4, max.Q = 4, max.d = 1, max.D = 1, start.p = 0, start.q = 0, start.P = 0, start.Q = 0, seasonal = TRUE, stationary = TRUE)
+sarima <- Arima(model1416$Order_Quantity, order=c(3,1,2), seasonal=list(order = c(1,1,2), period = 12))
+sarima %>% forecast(h=200) %>% autoplot + theme_stata()
